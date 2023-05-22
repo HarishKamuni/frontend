2d7708a665b28ecd90ab6c598694e7aa7f40c844
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2023-05-22 11:02:27 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import "../styles/App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  const fetchData = async () => {
+    try {
+        setLoading(true);
+        const response = await fetch('https://api.mfapi.in/mf');
+        const data = await response.json();
+            
+        const matchingFunds = data.filter(fund => {
+          const schemeName = fund.schemeName.toLowerCase();
+          // console.log(schemeName)
+          return schemeName.includes(search.toLowerCase());
+        }).slice(0, 10);
+        setData(matchingFunds);
+        setLoading(false);
+        
+      } catch (error) {
+        console.log(error);
+      }
+  }
+    useEffect(()=>{
+        fetchData();
+    }, [search])
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-container">
+        <input
+          type="text" 
+          placeholder="Search for a mutual fund"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+      </div>
+      <div className="funds-container">
+        {loading && <div>Loading...</div>}
+        <ul>
+          {data.map((data,inx) =>
+            <li key={inx}>
+                <h3>{data.schemeName}</h3>
+              <p>Scheme Code: { data.schemeCode }</p>
+            </li>
+          )}
+              
+            
+          </ul>
+        
+      </div>
     </div>
   );
 }
